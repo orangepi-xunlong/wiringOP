ae776d4eab5191ec787e8f6e055fd5f4da8a49b3
baiywt <baiywt_gj@163.com>
2025-03-31 16:14:23 +0800

    Fix alignment issue with H3 pwm base address for mmap

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -5195,7 +5195,7 @@ int wiringPiSetup (void)
 		case PI_MODEL_H3: case PI_MODEL_ZERO_2_W: case PI_MODEL_4A:
 		case PI_MODEL_ZERO_3_PLUS:
 	
-			sunxi_gpio_info_t.pwm = (uint32_t *)mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, sunxi_gpio_info_t.pwm_base_addr);
+			sunxi_gpio_info_t.pwm = (uint32_t *)mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, sunxi_gpio_info_t.pwm_base_addr & (0xfffff000));
 			if ((int32_t)(unsigned long)sunxi_gpio_info_t.pwm == -1)
 				return wiringPiFailure(WPI_ALMOST, "wiringPiSetup: mmap (PWM) failed: %s\n", strerror(errno));
 
